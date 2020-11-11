ffb2b0ff5af5d9e5574f3008cb07829dfb47ab06
1024sparrow <1024sparrow@gmail.com>
2020-11-11 10:31:37 +0300

    issue_1: test

---
+++ tests/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#./archiever.sh ../1.arch
+
+pushd src > /dev/null
+for i in 1
+do
+	../archiever.sh "$i".arch
+	mv "$i" "$i"_compiled
+	tmp=$(cat "$i".pro)
+	ok=false
+	../../compile.js "$i"_compiled/"$tmp" && ../archiever.sh "$i"_compiled && ok=true
+	if $ok
+	then
+		if [[ ! $(cat "$i"_compiled.arch) == $(cat "$i"_expected_result.arch) ]]
+		then
+			echo "NOT THE SAME"
+			vimdiff "$i"_compiled.arch "$i"_expected_result.arch
+		fi
+	fi
+	rm "$i"_compiled.arch
+	rm -rf "$i"_compiled
+done
+popd > /dev/null
