3022582bb3cd3e77cc0bcc85c87934474ef7067c
1024sparrow <1024sparrow@gmail.com>
2020-11-19 12:58:48 +0300

    issue_2: гамётки на дальнейшее развитие

--- src/body/master.js/t.js
+++ src/body/master.js/t.js
@@ -18,4 +18,7 @@
 Допустим, мы создали файл (исходник), разбили его в попдиректорию. Затем добавили туда (в ту поддиреторию) ещё одну цель. Что теперь с этим делать? JOIN-ить не понятно как:
 РЕШЕНИЕ: собираем несколько файлов (ровно так, как прописано __meta__ файле той диретории, которую мы схлопываем)
 	*/
+
+	// mergeDirs - join some subdirectories into one subdirectory with some targets in the same __meta__. Может быть полезно в случае, когда разные цели используют общие исходники.
+	// splitDirs - for subdirectory create separated subdirectory for each target
 }
